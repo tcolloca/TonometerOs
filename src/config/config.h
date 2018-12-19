7a5fd10edc468190774a4707187d26c64337e0f5
Tom Colloca <tomascllc94@gmail.com>
2018-12-19 18:27:35 -0300

    config file

---
+++ src/config/config.h
@@ -0,0 +1,12 @@
+/*
+ * CONFIG FILE
+ */
+#ifndef CONFIG_CONFIG_H_
+#define CONFIG_CONFIG_H_
+
+#define LOGGER_LEVEL   DEBUG
+
+#define AIR_PUMP_PORT 'b'
+#define AIR_PUMP_PIN   1
+
+#endif  // CONFIG_CONFIG_H_
